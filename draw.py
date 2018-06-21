b740cafa115de7f5bbfd3cab595b4d56bfc1030a
Robert Rak <robertrak@hotmail.com>
2018-06-21 18:24:13 -0400

    node colors added

--- draw.py
+++ draw.py
@@ -2,32 +2,35 @@ import math
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Circle, Label, LabelSet, ColumnDataSource
 from bokeh.palettes import Spectral8
 
 from graph import *
 
+WIDTH = 500
+HEIGHT = 500
+CIRCLE_SIZE = 30
+
 graph_data = Graph()
 graph_data.debug_create_test_data()
-print(graph_data.vertexes)
+graph_data.bfs(graph_data.vertexes[0])
+# print(graph_data.vertexes)
 
 N = len(graph_data.vertexes)
 node_indices = list(range(N))
-
-print('Node indices: ', node_indices)
 
 color_list = []
 for vertex in graph_data.vertexes:
     color_list.append(vertex.color)
 
-plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 500),
+plot = figure(title='Graph Layout Demonstration', x_range=(0, WIDTH), y_range=(0, HEIGHT),
               tools='', toolbar_location=None)
 
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
 graph.node_renderer.data_source.add(color_list, 'color')
-graph.node_renderer.glyph = Oval(height=25, width=25, fill_color='color')
+graph.node_renderer.glyph = Circle(size=CIRCLE_SIZE, fill_color='color')
 
 # this is drawing edges from start to end
 
@@ -40,16 +43,21 @@ for vertex in graph_data.vertexes:
         graph.edge_renderer.data_source.data['start'].append(int(edge.origin.value[-1]) - 1)
         graph.edge_renderer.data_source.data['end'].append(int(edge.destination.value[-1]) - 1)
 
-print(graph.edge_renderer.data_source.data)
-
 ### start of layout code
 x = [v.pos['x'] for v in graph_data.vertexes]
 y = [v.pos['y'] for v in graph_data.vertexes]
+name = [v.value for v in graph_data.vertexes]
+
+### create labels
+source = ColumnDataSource(data=dict(x=x, y=y, name=name))
+labels = LabelSet(x='x', y='y', text='name', level='overlay', source=source, render_mode='canvas', text_align='center', text_baseline='middle')
 
 graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
 plot.renderers.append(graph)
+plot.add_layout(labels) # remember to draw labels after circles
+
 
 output_file('graph.html')
 show(plot)
--- graph.py
+++ graph.py
@@ -1,3 +1,5 @@
+import random
+
 class Edge:
     def __init__(self, origin, destination):
         self.origin = origin
@@ -34,3 +36,23 @@ class Graph:
 
         self.vertexes.extend([debug_vertex_1, debug_vertex_2, debug_vertex_3, debug_vertex_4])
 
+    def bfs(self, start):
+        random_color = "#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+
+        queue = []
+        found = []
+
+        queue.append(start)
+        found.append(start)
+
+        start.color = random_color
+        print(start.color)
+        while len(queue) > 0:
+            v = queue[0]
+            for edge in v.edges:
+                if edge.destination not in found:
+                    found.append(edge.destination)
+                    queue.append(edge.destination)
+                    edge.destination.color = random_color
+            queue.pop(0)
+        return found
